580e53f35f7b6e45418a97d51a124d9805a927f1
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-04-15 15:12:03 -0400

    Updating the JS and css file

--- Assets/script.js/script.js
+++ Assets/script.js/script.js
@@ -2,16 +2,26 @@ let $searchButton = $("#searchBtn");
 let $cityName = $("#enterCity");
 let $searchHistory = $("#searchHistory");
 let $cityDetail = $("#cityDetail");
-let $fiveDay = $("#fiveDay")
-let currentDate = moment().format('l');
+let $fiveDay = $("#fiveDay");
+let $weatherContent = $("#weatherContent")
+let currentDate = moment().format("l");
+const cities = [];
 
+$weatherContent.hide();
 $searchButton.on("click", function () {
-  console.log($cityName.val());
+  $weatherContent.fadeOut();
+  $searchHistory.html('')
   let cityName = $cityName.val();
-  let $li = $("<li>");
-  $li.text(cityName);
-  $searchHistory.append($li);
-
+  cities.push(cityName);
+  const citiesLi = cities
+    .filter((value, index) => cities.indexOf(value) === index)
+    .map(function (city) {
+      let $li = $("<li>");
+      $li.text(city);
+      return $li;
+    });
+  $searchHistory.append(citiesLi);
+  $cityName.val('')
   let requestUrl =
     "https://app-weather-update.herokuapp.com/getLocations?location=" +
     cityName;
@@ -36,54 +46,81 @@ $searchButton.on("click", function () {
           return response.json();
         })
         .then(function (data) {
-          $cityDetail.html('')
-          console.log(data);
+          console.log(data)
+          $cityDetail.html("");
           let $h2 = $("<h2>");
           $h2.text(cityName);
           $cityDetail.append($h2);
           let $h3 = $("<h2>");
           $h3.text(currentDate);
           $cityDetail.append($h3);
-          let pEl = $("<p>")
-          pEl.text('Temp: ' + data.current.temp + String.fromCharCode(176) + "F")
-          $cityDetail.append(pEl)
-          let pE2 = $("<p>")
-          pE2.text('Wind: ' + data.current.wind_speed + " MPH")
-          $cityDetail.append(pE2)
-          let pE3 = $("<p>")
-          pE3.text('Humidity: ' + data.current.humidity + " %")
-          $cityDetail.append(pE3)
-          let pE4 = $("<p>")
-          pE4.text('UVI Index: ' + data.current.uvi)
-          $cityDetail.append(pE4)
-          var dataLength = 5
-          $fiveDay.html('')
-          for (i=0; i<dataLength; i++){
-            console.log(data.daily[i])
-            const $divDay = $("<div>", {class: "card"})
-            // let imgEl = $(`<img src='${data.daily[i].clouds}'>`)
-            //  http://openweathermap.org/img/wn/10d@2x.png
-            // $divDay.append(imgEl)
-            let h1El = $('<h1>')
-            const date = moment.unix(data.daily[i].dt).tz(data.daily[i].timezone).format('MM/DD/YYYY');
-            h1El.text(date)
-            $divDay.append(h1El)
-            $fiveDay.append($divDay)
-            let pEl = $("<p>")
-            pEl.text('Temp: ' + data.daily[i].temp.day + String.fromCharCode(176) + "F")
-            console.log(pEl)
-            $divDay.append(pEl)
-            $fiveDay.append($divDay)
-            let pE2 = $("<p>")
-            pE2.text('Wind: ' + data.daily[i].wind_speed + " MPH")
-            $divDay.append(pE2)
-            $fiveDay.append($divDay)
-            let pE3 = $("<p>")
-            pE3.text('Humidity: ' + data.daily[i].humidity + " %")
-            $divDay.append(pE3)
-            $fiveDay.append($divDay)
+          let pEl = $("<p>");
+          pEl.text(
+            "Temp: " + data.current.temp + String.fromCharCode(176) + "F"
+          );
+          $cityDetail.append(pEl);
+          let pE2 = $("<p>");
+          pE2.text("Wind: " + data.current.wind_speed + " MPH");
+          $cityDetail.append(pE2);
+          let pE3 = $("<p>");
+          pE3.text("Humidity: " + data.current.humidity + " %");
+          $cityDetail.append(pE3);
+          let uvClass = '';
+          let uiIndex = data.current.uvi;
+          if (uiIndex <=2){
+            uvClass = 'low'
           }
-
+          if (uiIndex >=3 && uiIndex <=5){
+            uvClass = 'moderate'
+          }
+           
+          if (uiIndex >=6){
+            uvClass = 'high'
+          }
+          let pE4 = $("<p>");
+          let label = $('<label>')
+          let span = $('<span>', { class: 'uvClass ' + uvClass })
+          label.text("UVI Index: ")
+          span.text(uiIndex);
+          pE4.append(label);
+          pE4.append(span)
+          $cityDetail.append(pE4);
+          var dataLength = 6;
+          $fiveDay.html("");
+          for (i = 1; i < dataLength; i++) {
+            console.log(data.daily[i]);
+            const $divDay = $("<div>", { class: "card" });
+            let h1El = $("<h2>");
+            const date = moment
+              .unix(data.daily[i].dt)
+              .tz(data.timezone)
+              .format("MM/DD/YYYY");
+            h1El.text(date);
+            $divDay.append(h1El);
+            $fiveDay.append($divDay);
+            let weatherPic = data.daily[i].weather[0].icon
+            let currentPicEl = $("<img>");
+            var weatherIcon = "http://openweathermap.org/img/wn/"+weatherPic+"@2x.png";
+            currentPicEl.attr('src', weatherIcon)
+            $divDay.append(currentPicEl);
+            $fiveDay.append($divDay);
+            let pEl = $("<p>");
+            pEl.text(
+              "Temp: " + data.daily[i].temp.day + String.fromCharCode(176) + "F"
+            );
+            console.log(pEl);
+            $divDay.append(pEl);
+            $fiveDay.append($divDay);
+            let pE2 = $("<p>");
+            pE2.text("Wind: " + data.daily[i].wind_speed + " MPH");
+            $divDay.append(pE2);
+            $fiveDay.append($divDay);
+            let pE3 = $("<p>");
+            pE3.text("Humidity: " + data.daily[i].humidity + " %");
+            $divDay.append(pE3);
+            $fiveDay.append($divDay);
+            $weatherContent.fadeIn();
+          }
         });
     });
 });
